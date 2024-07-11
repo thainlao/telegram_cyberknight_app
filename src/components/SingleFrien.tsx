e0b6b0764b64ed342d918a255445aca6a35c520a
Саша Носко <gozxctrashh@gmail.com>
2024-07-11 09:25:29 +0300

    welfkwjef

--- src/components/SingleFrien.tsx
+++ src/components/SingleFrien.tsx
@@ -1,14 +1,43 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { fetchUserData } from "../hoocs/fetchUserData";
+import { IUserData } from "../utils/types";
 
 export interface Props {
     friend: any;
 }
 
 const SingleFrien: React.FC<Props> = ({friend}) => {
+    const [singleUserData, setSingleUserData] = useState<IUserData | null>(null);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        const getUserData = async () => {
+            try {
+                const data = await fetchUserData(friend);
+                setSingleUserData(data);
+            } catch (err: any) {
+                setError(err.message);
+            }
+        };
+
+        getUserData();
+    }, [friend]);
+
+    if (error) {
+        return <p>Error: {error}</p>;
+    }
+
+    if (!singleUserData) {
+        return <p>Loading...</p>;
+    }
+
     return (
         <div className="single_friend">
             <div className="single_fren_text">
-                <h1>{friend}</h1>
+                <h1>{singleUserData.username}</h1>
+                <p>CBK Coins: {singleUserData.cbkCoins}</p>
+                <p>Friends: {singleUserData.friends.length}</p>
+                <p>Last Collected: {singleUserData.cbkCoins}</p>
             </div>
         </div>
     )
---
+++ src/hoocs/fetchUserData.ts
@@ -0,0 +1,15 @@
+import axios from 'axios';
+
+export const fetchUserData = async (username: string) => {
+    try {
+        const response = await axios.post('/api/get-user-by-username', { usernames: [username] });
+        if (response.data.success) {
+            return response.data.users[0]; // Assuming the response contains an array of users
+        } else {
+            throw new Error(response.data.message);
+        }
+    } catch (error) {
+        console.error('Error fetching user data:', error);
+        throw error;
+    }
+};
